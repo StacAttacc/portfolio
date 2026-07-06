24766d06b42a04a5517ef643ae64ca33ff2bc437
Anastasia <cookiedemons@outlook.com>
2026-07-05 22:35:22 -0400

    feat: adding project card component with tailwind and daisyui

---
+++ app/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  description?: string
+  link: string
+}>()
+
+function handleClick() {
+  navigateTo(props.link, {
+    open: {
+      target: '_blank'
+    }
+  })
+}
+</script>
+
+<template>
+  <div
+    class="bg-base-100 text-base-content border border-base-300 rounded-xl shadow-sm p-4 cursor-pointer"
+    @click="handleClick"
+  >
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
